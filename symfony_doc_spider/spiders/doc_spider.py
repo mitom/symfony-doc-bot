fbe8bd22fa1bfde2612e6e592a767a298c617a71
mitom <tamas.millian@gmail.com>
2014-02-26 11:22:37 +0100

    added parsing for adominition blocks with anchors

--- symfony_doc_spider/spiders/doc_spider.py
+++ symfony_doc_spider/spiders/doc_spider.py
@@ -38,15 +38,15 @@ class DocSpider(CrawlSpider):
         # Traverse every first level section
         for section in sel.xpath('div[@class="section"]'):
             items = items + self.parseSection(response, section, depth)
-            break
 
         return items
 
     def parseSection(self, response, section, depth):
         item = SectionItem()
+        items = []
 
         # split the url to get the *.html part then split it again to remove everything after the last dot
-        pageName = response.url.rsplit('/',1)[1].rsplit('.', 1)[0]
+        page_name = response.url.rsplit('/',1)[1].rsplit('.', 1)[0].replace('_', ' ')
         # When the depth is 1 we are parsing the main header of the page,
         # therefore we don't need the # part
         if depth == 1:
@@ -55,7 +55,7 @@ class DocSpider(CrawlSpider):
             # absolute url to the section (granted to be unique)
             item["id"] = response.url+ '#' + section.xpath('@id').extract()[0]
 
-        item["tags"] = [pageName.replace('_', ' ')]
+        item["tags"] = [page_name]
 
         # header of the section
         item["title"] = section.xpath("h%d//text()" % depth).extract()[0]
@@ -63,15 +63,32 @@ class DocSpider(CrawlSpider):
         # The text of the section html, whitespace stripped and single lined
         content = ' '.join(section.xpath('p//text()').extract()).strip().replace('\n', ' ')
         # The text of the notes html, whitespace stripped and single lined
-        content += ' '.join(section.xpath('div[@class="admonition-wrapper"]//p[@class="last"]//text()').extract()).strip().replace('\n', ' ')
+        content += ' '.join(section.xpath('div[@class="admonition-wrapper" and not(@id)]//p//text()').extract()).strip().replace('\n', ' ')
         item["content"] = content
 
+        items = items + self.parseAdmonitionAnchors(response.url, page_name, section)
         # add the section to the items
-        items = [item]
+        items.append(item)
 
         # Traverse every next level section
         depth += 1
         for subSection in section.xpath('div[@class="section"]'):
             items = items + self.parseSection(response, subSection, depth)
+
+        return items
+
+    def parseAdmonitionAnchors(self, baseUrl, page_name, section):
+        items = []
+
+        for admonition in section.xpath('div[@class="admonition-wrapper" and @id]'):
+            item = SectionItem()
+            # absolute url to the section (granted to be unique)
+            item["id"] = baseUrl+ '#' + admonition.xpath('@id').extract()[0]
+            item["tags"] = [page_name]
+            # header of the section, it must be joined due to the tags inside it
+            item["title"] = ''.join(admonition.xpath('*/p[contains(@class, "sidebar-title")]//text()').extract()).strip()
+            # The text of the section html, whitespace stripped and single lined
+            item["content"] = ' '.join(section.xpath('p//text()').extract()).strip().replace('\n', ' ')
+            items.append(item)
 
         return items
